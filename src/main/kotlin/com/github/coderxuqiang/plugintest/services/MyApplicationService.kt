d8bfb5938adba85af00a5ea308482ee55bd7816f
GitHub Action <action@github.com>
2022-04-15 10:34:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/coderxuqiang/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/coderxuqiang/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.coderxuqiang.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.coderxuqiang.plugintest.MyBundle
 
 class MyApplicationService {
 
